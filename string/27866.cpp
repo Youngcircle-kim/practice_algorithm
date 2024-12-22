bc16512e97f7865773a57e328e0d9891057dea6d
youngcircle-kim <dudghks021121@gmail.com>
2024-12-22 16:24:08 +0900

    simple string prob

---
+++ string/27866.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int a;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> s;
+    cin >> a;
+    cout << s[a-1];
+}
