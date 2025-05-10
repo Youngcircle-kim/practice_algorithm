fe4dec0da2b3392def6ad084f5de51c63707af1c
youngcircle-kim <dudghks021121@gmail.com>
2025-05-11 00:22:25 +0900

    Backtracking

--- 15xxx/15664.cpp
+++ 15xxx/15664.cpp
@@ -1,29 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,m;
-int arr[10];
+int n, m;
+int num[10];
 int seq[10];
-bool vis[10];
 void func(int cnt, int k){
-    if(cnt == m) {
+    if(cnt == m){
         for(int i = 0; i < m; i++) cout << seq[i] <<" ";
         cout << "\n";
         return ;
     }
+    int pre = 0;
     for(int i = k; i < n; i++){
-        seq[cnt] = i;
-        func(cnt+ 1, i);
+        if(pre != num[i]){
+            seq[cnt] = num[i];
+            pre = seq[cnt];
+            func(cnt+1, i+1);
+        }
     }
 }
 int main(void){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
-    //constraints
-    //1<=n,m<=8
-    //1<= i <= 10_000
+    // constraints
+    // 1 <= m <= n <= 8
     cin >> n >> m;
-    for(int i=0; i < n;i++) cin >> arr[i];
-    sort(arr, arr+n);
-    func(0,0); 
+    for(int i = 0; i < n; i++) cin >> num[i];
+    sort(num, num +n);
+    func(0,0);
 }
