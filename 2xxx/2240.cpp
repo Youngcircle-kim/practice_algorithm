f21d65d09f77df14b65a4443cf650085a19fe43c
youngcircle-kim <dudghks021121@gmail.com>
2025-06-24 14:57:29 +0900

    [BOJ] 동전 2 / G5 / 30m

---
+++ 2xxx/2240.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t, w;
+int a[1004];
+int d[1004][34];
+int main(void){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> t >> w;
+    a[0] = 0;
+    for(int i = 1; i <= t; i++) cin >> a[i];
+
+}
